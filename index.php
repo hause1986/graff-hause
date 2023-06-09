3fa7b0ff2763d471ef1d28c83f5989eed5abad90
hause1986 <hause1986@mail.ru>
2023-06-09 16:36:10 +0300

    bot

--- index.php
+++ index.php
@@ -1,24 +1,37 @@
 <?
 $class = __DIR__ . '/hause/class.php';
 $init = __DIR__ . '/hause/init.php';
+$arListTask = array(
+	'INFO'	=>	__DIR__ . '/hause/task/info.php',
+);
+
 require( $init );
 require( $class );
 
-
-
 $ghBot = new GHTelegramBot( '6278856035:AAGaiRkUVJwiLncrbz_9z4GUkPSWI6_BaTY' );
 $infoBot = $ghBot->getMe();
-/*
-echo '<pre>';
-print_r( $infoBot );
-echo '</pre>';
-*/
 if( !$infoBot['ok'] ){
 	die();
 }
 
-$arMessBot = $ghBot->getUp();
+foreach( $arListTask as $v ){
+	require( $v );
+}
 
+$arMessBot = $ghBot->getUp();
+if( !$arMessBot['ok'] || !count( $arMessBot['result'] ) ){
+	die();
+}
+
+foreach( $arMessBot['result'] as $v ){
+	$text = $v['message']['text'];
+	switch( $text ) {
+		case '/info':
+		case '/start':
+			
+		break;
+	}
+}
 
 
 echo '<pre>';
